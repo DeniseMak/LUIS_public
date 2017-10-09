6b6a06965d2cba86f4d41b87ab987d141d421d95
Dina Berry <32525982+v-geberr@users.noreply.github.com>
2017-10-09 08:57:36 -0700

    Update LUIS.examples.labels.post.js

--- nodeJs/query_log_to_label_batch/LUIS.examples.labels.post.js
+++ nodeJs/query_log_to_label_batch/LUIS.examples.labels.post.js
@@ -5,8 +5,8 @@ var path = require('path');
 const LUIS_label_jsonFile = './utterances.json';
 const inFile = path.join(__dirname, LUIS_label_jsonFile);
 
-const LUIS_subscriptionKey = "fb3488ba06614b4985c1baa7a0af0376";
-const LUIS_appId = "2cce2d6a-2500-4ce7-804c-51da84609209";
+const LUIS_subscriptionKey = "<subscription_key>";
+const LUIS_appId = "<app_id>";
 const LUIS_versionId = "0.1";
 const uri = "https://westus.api.cognitive.microsoft.com/luis/api/v2.0/apps/" + LUIS_appId + "/versions/" + LUIS_versionId + "/examples";
 
