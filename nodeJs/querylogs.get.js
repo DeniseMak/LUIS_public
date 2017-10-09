1d421fd123faad15981c5998181c06b39c01ff35
Dina Berry <32525982+v-geberr@users.noreply.github.com>
2017-10-09 08:58:29 -0700

    Update querylogs.get.js

--- nodeJs/querylogs.get.js
+++ nodeJs/querylogs.get.js
@@ -3,8 +3,8 @@ var fs = require('fs-extra');
 var path = require('path');
 
 
-const LUIS_subscriptionKey = "fb3488ba06614b4985c1baa7a0af0376";
-const LUIS_appId = "2cce2d6a-2500-4ce7-804c-51da84609209";
+const LUIS_subscriptionKey = "<subscription key>";
+const LUIS_appId = "<appid>";
 const LUIS_versionId = "0.1";
 
 const WriteFile = async (file, data ) => {
